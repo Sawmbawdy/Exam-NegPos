02ab1302aaec76040d43b627986bd6e6867aa149
Sawmbawdy <saiompatro12@gmail.com>
2025-07-30 12:46:13 -0400

    Add files via upload

---
+++ FindingAlphabets.py
@@ -0,0 +1,9 @@
+Input = input("Type something random: \n")
+aplhabets = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+boole = Input.isalpha()
+
+if boole == True:
+    print("You typed a string.")
+else:
+    print("This is not a string")
+    print("You did not type a string.")
---
+++ NegaOrPos.py
@@ -0,0 +1,8 @@
+iput = int(input("Enter a number: "))
+if iput >= 0:
+    if iput == 0:
+        print("The number is zero.")
+    else:
+        print("The number is positive.") 
+else:
+    print("The number is negative.")
